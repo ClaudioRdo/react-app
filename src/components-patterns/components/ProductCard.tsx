7796e599d3d8ae465deffff6b2f694dafb7bc4fe
Robledo Claudio <claudio.n.robledo@gmail.com>
2023-07-27 23:47:45 -0300

    compound-pattern

--- src/components-patterns/components/ProductCard.tsx
+++ src/components-patterns/components/ProductCard.tsx
@@ -1,26 +1,95 @@
+import { ReactElement, createContext, useContext } from 'react';
 import { useProdructs } from '../hooks/useProducts';
 import styles from '../styles/styles.module.css';
-//import noImage from '../assets/no-image.jpg';
+import noImage from '../assets/no-image.jpg';
 
-const ProductCard = () => {
+interface Props {
+    product: Product;
+    children?: ReactElement | ReactElement[];
+};
+
+interface Product {
+    id: string,
+    title: string,
+    img?: string
+};
+
+interface ProductButtons {
+    increaseBy: (value: number) => void;
+    counter: number;
+};
+
+interface ProductContexProps {
+    counter: number;
+    increaseBy: (value: number) => void;
+    product: Product;
+};
+
+const ProductContext = createContext({} as ProductContexProps);
+const { Provider } = ProductContext;
+
+export const ProductImage = ({ img = '' }) => {
+
+    const { product } = useContext(ProductContext);
+    let imgToShow: string;
+
+    if (img) {
+        imgToShow = img;
+    } else if (product.img) {
+        imgToShow = product.img;
+    } else {
+        imgToShow = noImage;
+    };
+
+    return (
+        <img className={styles.productImg} src={imgToShow} alt="Product image" />
+    );
+};
+
+export const ProductTitle = ({ title }: { title?: string }) => {
+
+    const { product } = useContext(ProductContext);
+
+    return (
+        <span className={styles.productDescription}>
+            {title ? title : product.title}
+        </span>
+    );
+};
+
+export const ProductButtons = () => {
+
+    const { increaseBy, counter } = useContext(ProductContext);
+
+    return (
+        <div className={styles.buttonsContainer}>
+            <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}> - </button>
+            <div className={styles.countLabel}> {counter} </div>
+            <button className={styles.buttonAdd} onClick={() => increaseBy(+1)}> + </button>
+        </div>
+    );
+};
+
+const ProductCard = ({ product, children }: Props) => {
 
     const { counter, increaseBy } = useProdructs();
 
     return (
-        <div className={styles.productCard}>
-            <img className={styles.productImg} src="./coffee-mug.png" alt="Coffe mug" />
+        <Provider value={{
+            counter,
+            increaseBy,
+            product
+        }}>
 
-            <span className={styles.productDescription}> Coffee mug </span>
-
-            <div className={styles.buttonsContainer}>
-                <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}> - </button>
-                <div className={styles.countLabel}> {counter} </div>
-                <button className={styles.buttonAdd} onClick={() => increaseBy(+1)}> + </button>
+            <div className={styles.productCard}>
+                {children}
+                {/* <ProductImage img={product.img} />
+            <ProductTitle title={product.title}/>
+            <ProductButtons increaseBy={ increaseBy } counter={ counter } /> */}
             </div>
+        </Provider>
 
-        </div>
-
-    )
-}
+    );
+};
 
 export default ProductCard
--- src/components-patterns/pages/ShoppingPage.tsx
+++ src/components-patterns/pages/ShoppingPage.tsx
@@ -1,10 +1,25 @@
-import ProductCard from '../components/ProductCard';
+import ProductCard, { ProductButtons, ProductImage, ProductTitle } from '../components/ProductCard';
+
+const product = {
+  id: '1',
+  title: 'Coffee mug',
+  img: './coffee-mug.png'
+}
+
 const ShoppingPage = () => {
   return (
-    <div>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      flexWrap: 'wrap'
+    }}>
       <h1>Shopping Store</h1>
       <hr />
-      <ProductCard />
+      <ProductCard product={ product } >
+        <ProductImage />
+        <ProductTitle title=''/>
+        <ProductButtons />
+      </ProductCard>
     </div>
   )
 }
