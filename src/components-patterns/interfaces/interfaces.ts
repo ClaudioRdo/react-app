9f2c19516d9cb7dae5fef20754cac48f22364b3a
Robledo Claudio <claudio.n.robledo@gmail.com>
2023-08-03 13:31:31 -0300

    component extends styles ready

--- src/components-patterns/interfaces/interfaces.ts
+++ src/components-patterns/interfaces/interfaces.ts
@@ -1,9 +1,7 @@
-import { ReactElement } from "react";
-
-export interface ProductCardProps {
-    product: Product;
-    children?: ReactElement | ReactElement[];
-};
+import { Props as ProductButtonsProps } from '../components/ProductCard';
+import { Props as ProductCardProps } from '../components/ProductCard';
+import { Props as ProductImageProps } from '../components/ProductImage';
+import { Props as ProductTitleProps } from '../components/ProductTitle';
 
 export interface Product {
     id: string,
@@ -20,4 +18,11 @@ export interface ProductContexProps {
     counter: number;
     increaseBy: (value: number) => void;
     product: Product;
+};
+
+export interface ProductCardHOCProps {
+    ({ children, product }: ProductCardProps): JSX.Element,
+    Title: (Props: ProductTitleProps) => JSX.Element,
+    Image: (Props: ProductImageProps) => JSX.Element,
+    Buttons: (Props: ProductButtonsProps) => JSX.Element
 };
